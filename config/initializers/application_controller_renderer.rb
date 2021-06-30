1f9d0eefa910cf6d300721aaf1898a8363b8cbff
thongth <thongth@THONGTH>
2021-06-30 14:19:20 +0700

    update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '72998f8978e9d1ef8a2035bb36d13e87f56b16c00d6bdd15ddce7fc4982b64142440a6e6e38d4c3f66074b788e60b6c569c811346f9ad8a589f02c074f79809d'
