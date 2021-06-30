a476bdbe6861afc4582e3efd5b41d022c71ac642
thongth <thongth@THONGTH>
2021-06-30 17:00:54 +0700

    up

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @recipes = Recipe.all
   end
@@ -14,19 +16,30 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
-    @recipe.save
-    redirect_to recipe_path(@recipe)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
 
   def edit
     @recipe = Recipe.find(params[:id])
+    if @recipe.user != current_user
+      redirect_to recipes_path, alert: "不正なアクセスです。"
+    end
   end
 
   def update
     @recipe = Recipe.find(params[:id])
     @recipe.update(recipe_params)
 
-    redirect_to recipe_path(@recipe)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+   
   end
 
   def destroy
@@ -37,6 +50,7 @@ class RecipesController < ApplicationController
   end
 
   private
+
   def recipe_params
     params.require(:recipe).permit(:title, :body, :image)
   end
