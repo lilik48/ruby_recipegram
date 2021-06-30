58c5beff1647df9042f22c0adbc8bcf6f0595c0c
thongth <thongth@THONGTH>
2021-06-30 16:23:30 +0700

    update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,6 +29,13 @@ class RecipesController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+
+    redirect_to recipes_path
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:title, :body, :image)
